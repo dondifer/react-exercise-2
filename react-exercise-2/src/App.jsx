5eb1ff39f62a93e8865f11b3d52c411f813100a4
dondifer <dondifer@gmail.com>
2023-06-09 20:21:44 +0200

    feat: dish descrition

--- react-exercise-2/src/App.jsx
+++ react-exercise-2/src/App.jsx
@@ -6,20 +6,23 @@ function App() {
   const dishes = [
     {
       id: 1,
-      name: "plato 1",
-      description: "Este es el plato 1",
+      name: "Carbonara",
+      description:
+        "La carbonara è un primo piatto tipico della tradizione romana preparato con un condimento a base di uova, guanciale e pecorino romano.",
       price: 10,
     },
     {
       id: 2,
-      name: "plato 2",
-      description: "Este es el plato 2",
+      name: "Ricotta alle fungi",
+      description:
+        "La ricotta è un latticino dall'aspetto piuttosto denso e granuloso. ",
       price: 20,
     },
     {
       id: 3,
-      name: "plato 3",
-      description: "Este es el plato 3",
+      name: "Focaccia",
+      description:
+        "La focaccia è un pane basso, composto da un impasto di farina, acqua, sale e lievito, che può essere cotto sia al forno che alla brace",
       price: 15,
     },
   ];
